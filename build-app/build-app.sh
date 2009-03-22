b594d3e00365c4d36af0f75d29bcc478d2472491
Etienne Perot <etienne@perot.me>
2009-03-22 18:02:55 +0000

    Added auto version rename to OS X build script

--- build-app/build-app.sh
+++ build-app/build-app.sh
@@ -3,7 +3,7 @@
 echo "Starting build process."
 cd ./..
 hdiutil detach /Volumes/DamnVid
-rm -rf DamnVid.app required-files.txt DamnVid.dmg
+rm -rf DamnVid.app required-files.txt DamnVid-*.dmg
 echo "Compyling..."
 python build-required-files.py
 python ./build-app/py2app-osx.py py2app
@@ -13,5 +13,7 @@ rm -rf build dist
 chmod 0777 ./DamnVid.app/Contents/Resources/bin/ffmpegosx
 echo "Building disk image..."
 ./build-app/buildDMG.pl -dmgName DamnVid -volName DamnVid -buildDir ./ -deleteHeaders -compressionLevel 9 ./DamnVid.app
+read version<version.damnvid
+mv DamnVid.dmg DamnVid-$version.dmg
 rm -rf ./DamnVid.app ./sh ./required-files.txt
 echo "All done!"
