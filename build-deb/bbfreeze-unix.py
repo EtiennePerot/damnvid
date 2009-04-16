35dbdf8946e02ec21ab26f109bba3ebaafe43c8a
Etienne Perot <etienne@perot.me>
2009-04-16 06:49:41 +0000

    Added bbfreeze python script

---
+++ build-deb/bbfreeze-unix.py
@@ -0,0 +1,7 @@
+from bbfreeze import Freezer
+
+bbFreeze_Class=Freezer('package')
+bbFreeze_Class.addScript('DamnVid.py',gui_only=True)
+bbFreeze_Class.use_compression=1
+bbFreeze_Class.include_py=True
+bbFreeze_Class()
