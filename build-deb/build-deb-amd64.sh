830dfd08582e56476ce8752d4a68c8a872e0ecf5
Etienne Perot <etienne@perot.me>
2009-08-22 02:12:10 +0000

    Version 1.0.3 release, overdue better unicode support

--- build-deb/build-deb-amd64.sh
+++ build-deb/build-deb-amd64.sh
@@ -3,9 +3,9 @@
 # Yes, I am aware that this is absolutely not the right way to build .deb's. But hey, if you wanna help me build .deb's, feel free! :)
 
 origcurdir=$(pwd)
-mv ../bin/ffmpeg ../bin/ffmpeg-i386-temp
-mv ../bin/ffmpeg-amd64 ../bin/ffmpeg
+mv ../bin/ffmpeg ../bin/ffmpeg32
+mv ../bin/ffmpeg64 ../bin/ffmpeg
 sh build-deb.sh
 cd $origcurdir
-mv ../bin/ffmpeg ../bin/ffmpeg-amd64
-mv ../bin/ffmpeg-i386-temp ../bin/ffmpeg
+mv ../bin/ffmpeg ../bin/ffmpeg64
+mv ../bin/ffmpeg32 ../bin/ffmpeg
