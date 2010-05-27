949783af13b67fc9129335dab79898919f41bab7
Andreas Noteng <anoteng@gmail.com>
2010-05-27 20:35:43 +0000

    Delete files not needed for Lucid packaging

--- build-deb/bbfreeze-unix.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from bbfreeze import Freezer
-
-bbFreeze_Class=Freezer('package')
-bbFreeze_Class.addScript('DamnVid.py',gui_only=True)
-bbFreeze_Class.use_compression=1
-bbFreeze_Class.include_py=True
-bbFreeze_Class()
