28bcd4100721e870e360bd6b2e4ada80a1e80fba
Etienne Perot <etienne@perot.me>
2010-06-21 22:30:15 +0000

    Added break statements in find-system-paths loop to avoid looking further

--- build-exe/find-system-paths-win32.py
+++ build-exe/find-system-paths-win32.py
@@ -1,16 +1,17 @@
 import os, sys
 
 paths={
-   'nsis': ['C:\Program Files\NSIS', 'C:\Program Files (x86)\NSIS'],
-   'python32': ['C:\Python25-x86', 'C:\Python26-x86', 'C:\Python25', 'C:\Python26'],
-   'python64': ['C:\Python25', 'C:\Python26']
+	'nsis': ['C:\Program Files\NSIS', 'C:\Program Files (x86)\NSIS'],
+	'python32': ['C:\Python25-x86', 'C:\Python26-x86', 'C:\Python25', 'C:\Python26'],
+	'python64': ['C:\Python25', 'C:\Python26']
 }
 
 for k in paths.iterkeys():
-   found = False
-   for i in paths[k]:
-      if os.path.exists(i):
-         open(k+'-path.tmp', 'w').write(i)
-         found = True
-   if not found:
-       print >> sys.stderr, 'Error: Could not find '+k+', please edit find-system-paths.py.'
+	found = False
+	for i in paths[k]:
+		if os.path.exists(i):
+			open(k+'-path.tmp', 'w').write(i)
+			found = True
+			break
+	if not found:
+		print >> sys.stderr, 'Error: Could not find '+k+', please edit find-system-paths.py.'
